f40f1849f96262af43867b7c79334dbaa7bfad06
Nikky1092 <mariannaniko1092@gmail.com>
2023-03-15 17:08:29 +0000

    Domain Name Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let newArray = [];
+  for (let i = 0; i < pronoun.length; i++) {
+    let pronounValue = pronoun[i];
+    for (let j = 0; j < adj.length; j++) {
+      let adjValue = adj[j];
+      for (let k = 0; k < noun.length; k++) {
+        let nounValue = noun[k];
+        let domain = pronounValue + adjValue + nounValue + ".com";
+        newArray.push(domain);
+      }
+    }
+  }
+  function getList(newArray, separator) {
+    var text = newArray.join(separator);
+    document.getElementById("name").innerHTML = text;
+  }
+  var separator = "<br>";
+  getList(newArray, separator);
 };
